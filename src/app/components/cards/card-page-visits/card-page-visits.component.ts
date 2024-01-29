2a0d98c0eeba80b7c10312ea1ad0e10cad411c4a
moussa223 <mm6824963@gmail.com>
2024-01-29 12:12:32 +0100

    Affichage du nombre total d'objet et d'emprunt dans le comopsant dashboard et ajout d'un bouton profil et deconnecter

--- src/app/components/cards/card-page-visits/card-page-visits.component.ts
+++ src/app/components/cards/card-page-visits/card-page-visits.component.ts
@@ -1,11 +1,46 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import {EmpruntService} from '../../maps/map-example/emprunt.service';
+import {SettingService} from '../../../views/admin/settings/setting.service';
 
 @Component({
-  selector: "app-card-page-visits",
-  templateUrl: "./card-page-visits.component.html",
+  selector: 'app-card-page-visits',
+  templateUrl: './card-page-visits.component.html',
 })
 export class CardPageVisitsComponent implements OnInit {
-  constructor() {}
+  EmpruntData: any; // Contient les infos de tous les objets de la Base de données Mydil
+  ProductData: any; // Contient les infos de tous les objets de la Base de données Mydil
+  constructor(public empruntService:EmpruntService,public settingService:SettingService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getAllEmprunts();
+    this.getAllProducts();
+  }
+  // ----------------- get All Emprunts-------------
+  getAllEmprunts(){
+    this.empruntService.getAllEmprunts().subscribe(
+      (Emprunts) => {
+        // Vous pouvez utiliser les données des étudiants ici
+        console.log('les emprunts: '+Emprunts);
+        this.EmpruntData = Emprunts;
+      },
+      (error) => {
+        // Gérez les erreurs ici
+        console.error(error);
+      }
+    );
+  }
+  // ----------------- get All Products-------------
+  getAllProducts(){
+    this.settingService.getAllProducts().subscribe(
+      (Products) => {
+        // Vous pouvez utiliser les données des étudiants ici
+        console.log(Products);
+        this.ProductData = Products;
+      },
+      (error) => {
+        // Gérez les erreurs ici
+        console.error(error);
+      }
+    );
+  }
 }
